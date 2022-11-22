448167aa8b269ff3f764d2c873623529ae8cd537
Aryan Chouksey <96980212+MrNoBody86@users.noreply.github.com>
2022-11-22 22:19:41 +0530

    Add files via upload

---
+++ C++/simplecalc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x , y;
+    cout<< "Type first number: ";
+    cin>>x;
+    cout<<"\nType second number: ";
+    cin>>y;
+    cout<<"\nYour added number is: "<<x+y;
+    return 0;
+}
